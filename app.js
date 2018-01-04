c0b442d8ba5a89671ad40c77b94e8fb07328d2a0
Eddy Caldas <eddycaldas@live.com>
2018-01-04 15:18:00 -0700

    examples

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const name = 'John';
 const age = '30';
 const job = 'Web developer';
 const city = 'Denver';
+
+function hello() {
+    return 'Hello there!'
+}
 let html;
 
 html = `
@@ -12,6 +16,9 @@ html = `
 <li>Age: ${age}</li>
 <li>Job: ${job}</li>
 <li>City: ${city}</li>
+<li>${2 + 2}</li>
+<li>${hello()}</li>
+<li>${age > 30 ? 'Over 30' : 'Under 30'}</li>
 </ul>
 `;
 
